f38d15cbe05650bd1b7d81f1da2d26230d36a23e
prasenjeetbansode <34614555+prasenjeetbansode@users.noreply.github.com>
2023-05-27 08:08:49 +0530

    Add files via upload

---
+++ PP_bubble_sort.cpp
@@ -0,0 +1,65 @@
+
+#include<iostream>
+#include<stdlib.h>
+#include<omp.h>
+using namespace std;
+
+void bubble(int *, int);
+void swap(int &, int &);
+
+
+void bubble(int *a, int n)
+{
+    for(  int i = 0;  i < n;  i++ )
+     {
+   	 int first = i % 2;
+
+   	 #pragma omp parallel for shared(a,first)
+   	 for(  int j = first;  j < n-1;  j += 2  )
+   	   {
+   		 if(  a[ j ]  >  a[ j+1 ]  )
+   		  {
+     			 swap(  a[ j ],  a[ j+1 ]  );
+   		  }
+   		   }
+     }
+}
+
+
+void swap(int &a, int &b)
+{
+
+    int test;
+    test=a;
+    a=b;
+    b=test;
+
+}
+
+int main()
+{
+
+    int *a,n;
+    cout<<"\n enter total no of elements=>";
+    cin>>n;
+    a=new int[n];
+    cout<<"\n enter elements=>\n";
+    for(int i=0;i<n;i++)
+    {
+   	 cin>>a[i];
+    }
+double startTime = omp_get_wtime();
+    bubble(a,n);
+double endTime = omp_get_wtime();
+double executionTime = endTime - startTime;
+        cout<<"\n sorted array is=>\n";
+
+    for(int i=0;i<n;i++)
+    {
+   	 cout<<a[i]<<endl;
+    }
+cout << "Execution time: " << executionTime << " seconds" << endl;
+
+
+return 0;
+}
